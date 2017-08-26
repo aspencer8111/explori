66aa9763a27ed9493af31a4ae202092bec2f562d
Keri Spencer <kerigspencer@gmail.com>
2017-08-26 19:34:50 -0400

    Update Home CSS

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
-import '../styles/App.css';
+import React, { Component } from 'react'
+import '../styles/App.css'
 import { Link } from 'react-router-dom'
 
 export default class BaseLayout extends Component {
   render() {
     return (
-      <div className="App">
-        <div>
-      {this.props.children}
-    </div>
-    
+      <div className="flexWrap">
+        <div className="App">
+          <div className="whiteBox">
+            {this.props.children}
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 import '../styles/App.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import logo from '../imgs/Explorius-Logo.png'
 import Facebook from './Facebook'
 
-export default class Home extends Component
-{
+export default class Home extends Component {
   constructor(props) {
     super(props)
   }
@@ -13,15 +12,17 @@ export default class Home extends Component
   render() {
     return (
       <div className="home">
-      <img src={logo} alt="" id="logo" />
-      <h2>Book your excursions</h2>
-      <h4>Log in for your customized cruise experience</h4>
-      <form>
-        <Facebook />
-        <Link to="/loading">
-        <input type="submit" value="Let's Go" />
-        </Link>
-      </form>
+        <img src={logo} alt="" id="logo" />
+        <h2>Book your excursions.</h2>
+        <h4>LOG IN FOR YOUR CUSTOMIZED CRUISE EXPERIENCE</h4>
+        <form>
+          <Facebook />
+          <Link to="/loading">
+            <div className="submitButton">
+              <input type="submit" value="Let's Go" />
+            </div>
+          </Link>
+        </form>
       </div>
     )
   }
