62447c82f76da81e893691f3a325eb13f49b918e
Alex Spencer <alex@alexspencer.net>
2017-08-26 21:37:09 -0400

    update results component

--- src/components/Results.js
+++ src/components/Results.js
@@ -8,10 +8,29 @@ export default class Results extends Component {
     data: []
   }
 
+  parseDuration(duration){
+    if(duration < 2){
+      return "Less than 2 Hours"
+    } else if(duration > 2 && duration <= 3){
+      return "2 to 3 hours"
+    } else {
+      return "3 to 5 hours"
+    }
+  }
+
+  parsePriceRange(price_range){
+    if(price_range == 1){
+      return 'Less than $50'
+    } else if(price_range == 2){
+      return '$50 to $100'
+    } else {
+      return 'more than $100'
+    }
+  }
+
 
   _submit = (e) => {
     e.preventDefault()
-
     console.log(e.target.name)
   }
 
@@ -24,8 +43,12 @@ export default class Results extends Component {
   render() {
     let exc = this.state.data.slice(0, 3).map(excursion => {
       return (
-        <div>
-          <p>{excursion.title}</p>
+        <div key={excursion.id}>
+          <h3>{excursion.title}</h3>
+          <p>Activity Level: {excursion.activity_level}</p>
+          <p>Price Range: {this.parsePriceRange(excursion.price_range)}</p>
+          <p>Duration: {this.parseDuration(excursion.duration)}</p>
+          <p>{excursion.desc}</p>
           <form>
             <Link to="/info">
             <input type="button" onClick={this._submit} value="Learn More" name={excursion.id} />
@@ -37,7 +60,7 @@ export default class Results extends Component {
     })
     return (
       <div>
-        <h1>{exc}</h1>
+        {exc}
       </div>
     )
   }
